4133e47fd6da43481f49462a9ac1a560ccf4f392
ehddnr301 <dy950328@gmail.com>
2020-02-09 19:07:26 +0900

    서서히 껏다키기

--- app.ino
+++ app.ino
@@ -1,27 +1,20 @@
+int i = 0;
 
 void setup()
 {
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(13, OUTPUT);
+    pinMode(6, OUTPUT);
 }
 
 void loop()
 {
-    digitalWrite(13, HIGH);
-    digitalWrite(3, HIGH);
-    delay(1000);
-    digitalWrite(3, LOW);
-    delay(1000);
-    digitalWrite(4, HIGH);
-    delay(1000);
-    digitalWrite(4, LOW);
-    delay(1000);
-    digitalWrite(5, HIGH);
-    delay(1000);
-    digitalWrite(5, LOW);
-    delay(1000);
-    digitalWrite(13, LOW);
-    delay(2000);
+    for (i = 255; i >= 0; i -= 1)
+    {
+        analogWrite(6, i);
+        delay(10);
+    }
+    for (i = 0; i <= 255; i += 1)
+    {
+        analogWrite(6, i);
+        delay(10);
+    }
 }
